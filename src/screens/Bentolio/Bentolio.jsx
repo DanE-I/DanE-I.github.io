708b84567848193607736d7f138d16df655caf02
DAN <odefosa-idehen@upei.ca>
2024-11-05 19:02:37 -0400

    test branch commit

---
+++ src/screens/Bentolio/Bentolio.jsx
@@ -0,0 +1,136 @@
+import React from "react";
+import "./style.css";
+
+export const Bentolio = () => {
+  return (
+    <div className="bentolio">
+      <div className="div">
+        <div className="SOCIALS">
+          <div className="LINKS">
+            <div className="text-wrapper">INSTAGRAM</div>
+
+            <div className="text-wrapper">TWITTER</div>
+
+            <div className="text-wrapper">LINKEDIN</div>
+          </div>
+        </div>
+
+        <div className="CONTACT">
+          <div className="text-wrapper-2">Contact me</div>
+
+          <div className="TOP-BAR">
+            <div className="have-some-questions">
+              Have some
+              <br />
+              questions?
+            </div>
+
+            <img
+              className="ARROW"
+              alt="Arrow"
+              src="https://c.animaapp.com/pFe2ZkhT/img/arrow.svg"
+            />
+          </div>
+        </div>
+
+        <div className="ABOUT">
+          <p className="p">
+            Julia Huang is an innovative AI artist, renowned for blending
+            cutting-edge technology with creative expression. Based in LA, she
+            crafts unique digital art experiences accessible globally.
+          </p>
+
+          <img
+            className="CIRCLE-ICON"
+            alt="Circle ICON"
+            src="https://c.animaapp.com/pFe2ZkhT/img/circle-icon.svg"
+          />
+        </div>
+
+        <div className="WORK">
+          <div className="ITEM">
+            <div className="text-wrapper-3">Zephyr</div>
+          </div>
+
+          <div className="ITEM-2">
+            <img
+              className="BORDER"
+              alt="Border"
+              src="https://c.animaapp.com/pFe2ZkhT/img/border.svg"
+            />
+
+            <div className="text-wrapper-3">Verve</div>
+          </div>
+
+          <div className="ITEM-3">
+            <img
+              className="img"
+              alt="Border"
+              src="https://c.animaapp.com/pFe2ZkhT/img/border-1.svg"
+            />
+
+            <div className="text-wrapper-3">Elara</div>
+          </div>
+
+          <div className="ITEM-4">
+            <img
+              className="BORDER-2"
+              alt="Border"
+              src="https://c.animaapp.com/pFe2ZkhT/img/border-2.svg"
+            />
+
+            <div className="IMAGE" />
+
+            <img
+              className="ARROW-ICON"
+              alt="Arrow ICON"
+              src="https://c.animaapp.com/pFe2ZkhT/img/arrow-icon.svg"
+            />
+
+            <div className="text-wrapper-3">Musea</div>
+          </div>
+        </div>
+
+        <div className="PORTRAIT" />
+
+        <div className="SLOGAN-INTRO">
+          <p className="artist-redefining">
+            <span className="span">Artist Redefining </span>
+
+            <span className="text-wrapper-4">Architecture</span>
+
+            <span className="span"> with AI-Driven Design</span>
+          </p>
+
+          <img
+            className="FLOWER-ICON"
+            alt="Flower ICON"
+            src="https://c.animaapp.com/pFe2ZkhT/img/flower-icon.svg"
+          />
+        </div>
+
+        <header className="HEADER">
+          <div className="NAV">
+            <p className="LOGO">
+              <span className="text-wrapper-4">JULIA</span>
+
+              <span className="text-wrapper-5">&nbsp;</span>
+
+              <span className="text-wrapper-6">HUANG</span>
+            </p>
+
+            <div className="PAGE-LINKS">
+              <div className="frame">
+                <div className="text-wrapper-7">PROJECTS</div>
+
+                <div className="text-wrapper-7">ABOUT</div>
+
+                <div className="text-wrapper-7">CONTACT</div>
+              </div>
+            </div>
+          </div>
+        </header>
+      </div>
+    </div>
+  );
+};
